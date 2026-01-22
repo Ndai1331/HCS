a52cbbdfc25dcf84b2155e9974d9512792590ee9
tinngo95 <ngotrungtin0@gmail.com>
2026-01-22 10:41:41 +0700

    Update

---
+++ src/HC.Blazor/Components/Pages/NewIndex.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Web;
+using Blazorise;
+using Blazorise.DataGrid;
+using Volo.Abp.BlazoriseUI.Components;
+using Microsoft.AspNetCore.Authorization;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Components.Web.Theming.PageToolbars;
+using HC.Workflows;
+using HC.Permissions;
+using HC.Shared;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Volo.Abp;
+using Volo.Abp.Content;
+using Microsoft.Extensions.Caching.Memory;
+using HC.Projects;
+using HC.ProjectTasks;
+using HC.CalendarEvents;
+using HC.Documents;
+using HC.NotificationReceivers;
+
+namespace HC.Blazor.Components.Pages;
+
+public partial class NewIndex
+{
+    [Inject]
+    protected NavigationManager Navigation { get; set; } = default!;
+
+    private void Login()
+    {
+        Navigation.NavigateTo("/Account/Login", true);
+    }
+}
