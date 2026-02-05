6045482cc2d6764b5302c2e5ba068188f272e7e0
NDai <longnguyen13th1d.pou@gmail.com>
2026-02-05 19:15:32 +0700

    update

--- src/HC.AuthServer/wwwroot/global-scripts.js
+++ src/HC.AuthServer/wwwroot/global-scripts.js
@@ -1,1 +1,10 @@
 /* Your Global Scripts */
+
+//append style to body
+const style = document.createElement('style');
+style.textContent = `
+    .lpx-login-bg{
+        background-image: url('https://dev.benhvien199.vn/images/login-pages/login-bg-img-light.svg') !important;
+    }
+`;
+document.body.appendChild(style);
