2c39d51dc6cb7a72a4542393acdb3b17a59dba9a
NDai <longnguyen13th1d.pou@gmail.com>
2026-04-21 17:46:59 +0700

    update

---
+++ src/HC.BackgroundJobWorker/HCBackgroundJobWorkerModule.cs
@@ -0,0 +1,8 @@
+using Volo.Abp.Modularity;
+
+namespace HC.BackgroundJobWorker;
+
+public class HCBackgroundJobWorkerModule : AbpModule
+{
+
+}
---
+++ src/HC.BackgroundJobWorker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+
+namespace HC.BackgroundJobWorker;
+
+public class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        using (var application = await AbpApplicationFactory.CreateAsync<HCBackgroundJobWorkerModule>())
+        {
+            await application.InitializeAsync();
+
+            await application.ServiceProvider
+                .GetRequiredService<HCBackgroundJobWorkerService>()
+                .RunAsync(args);
+            
+            await application.ShutdownAsync();
+        }
+    }
+}
